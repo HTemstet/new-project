1e9a1dc6e557eb9c2173d3eb07a641ab6cb2b992
Hadas <hadas80030@gmail.com>
2021-12-26 16:41:20 +0200

    Thrid commit

--- client/src/app/Components/about-employer/about-employer.component.ts
+++ client/src/app/Components/about-employer/about-employer.component.ts
@@ -12,6 +12,7 @@ import { Button } from 'primeng/button';
   templateUrl: './about-employer.component.html',
   styleUrls: ['./about-employer.component.css']
 })
+
 export class AboutEmployerComponent implements OnInit {
   constructor(private RequestServ:RequestService,
     private AreaServ:AreaService,
@@ -19,27 +20,16 @@ export class AboutEmployerComponent implements OnInit {
     private ValidationServ:ValidationService,
     private myrouter:Router,
     private GlobalServ:GlobalService) { }
+
   ngOnInit() {
   }
   colorButtonOver()
-  {
+  { 
     document.querySelector("button").style.backgroundColor = "rgb(245, 20, 12)";
     document.querySelector("button").style.color = "white";
     document.querySelector("button").style.borderColor = "white";
   }
   colorButtonOut()
-  {
-    document.querySelector("button").style.backgroundColor = "white";
-    document.querySelector("button").style.color = "#34495e";
-    document.querySelector("button").style.borderColor = "rgb(245, 20, 12)";
-  }
-  colorButtonOver1()
-  {
-    document.querySelector("button").style.backgroundColor = "rgb(245, 20, 12)";
-    document.querySelector("button").style.color = "white";
-    document.querySelector("button").style.borderColor = "white";
-  }
-  colorButtonOut1()
   {
     document.querySelector("button").style.backgroundColor = "white";
     document.querySelector("button").style.color = "#34495e";
