107dd7337f53a1ff763d61d1a9715c7cda13dfbb
leah209309 <64496133+leah209309@users.noreply.github.com>
2022-01-02 20:10:54 +0200

    02012022

--- client/src/app/OtherPages/pending-changes.ts
+++ client/src/app/OtherPages/pending-changes.ts
@@ -9,12 +9,8 @@ export interface ComponentCanDeactivate {
 @Injectable()
 export class PendingChangesGuard implements CanDeactivate<ComponentCanDeactivate> {
   canDeactivate(component: ComponentCanDeactivate): boolean | Observable<boolean> {
-    // if there are no pending changes, just allow deactivation; else confirm first
     return component.canDeactivate() ?
       true :
-      // NOTE: this warning message will only be shown when navigating elsewhere within your angular app;
-      // when navigating away from your angular app, the browser will show a generic warning message
-      // see http://stackoverflow.com/a/42207299/7307355
       confirm('אזהרה! בעמוד זה ישנם שינויים שטרם שמרת. יציאה מהעמוד תבטל אותם. האם אתה בטוח שברצונך לצאת?');
   }
-}
+} ;
